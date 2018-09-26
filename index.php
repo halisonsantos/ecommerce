c8b877f46ae43a146f2f47b8123916722a1c61a3
Halison Santos <halisonbatista@gmail.com>
2018-09-26 10:18:05 -0300

    erro na recuperação de senha

--- index.php
+++ index.php
@@ -62,6 +62,173 @@ $app->get('/admin/logout', function(){
 	exit;
 
 });
+#listar usuários
+$app->get("/admin/users", function(){
+	#verificar se usuário está logado e tem acesso ao administrativo
+	User::verifyLogin();
+
+	#Método statico pra listar
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users",array(
+		"users"=>$users
+
+	));
+
+});
+#create .. get tras o template
+$app->get("/admin/users/create", function(){
+	#verificar se usuário está logado e tem acesso ao administrativo
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+#deletar usuário(obs: o delete tem q ficar antes do update pq o slim framework vai ver q tem o /delete primeiro, se ficar em baixo o slim vai entender q é tudo tudo q vem depois do iduser faz parte do id)
+$app->get("/admin/users/:iduser/delete", function($iduser){
+	#verificar se usuário está logado e tem acesso ao administrativo
+	User::verifyLogin();
+	#carregar o usuários para ter certeza que ele existe no banco
+	$user = new User();
+	
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
+
+});
+
+#update .. vai passar o id do usuário
+$app->get("/admin/users/:iduser", function($iduser){
+	#verificar se usuário está logado e tem acesso ao administrativo
+	User::verifyLogin();
+
+	$user = new User();
+ 	#fazendo um get passando o número do id
+	$user->get((int)$iduser);
+ 
+  	$page = new PageAdmin();
+ 	#passar um array
+   	$page ->setTpl("users-update", array(
+        "user"=>$user->getValues()
+   	));
+});
+#criar a rota pra salvar quando é post vai fazer o insert dos dados
+$app->post("/admin/users/create", function(){
+	#verificar se usuário está logado e tem acesso ao administrativo
+	User::verifyLogin();
+
+	$user = new User();
+	#se o iadmin for definido o valor dele é 1 se não é 0
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+	#execultar o insert no banco
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+#salvar a edição
+$app->post("/admin/users/:iduser", function($iduser){
+	#verificar se usuário está logado e tem acesso ao administrativo
+	User::verifyLogin();	
+
+	$user = new User();
+	#se o iadmin for definido o valor dele é 1 se não é 0
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	#carregar os dados passando o id
+	$user->get((int)$iduser);
+	#setar os dados do post
+	$user->setData($_POST);
+	#fazer o update
+	$user->update();
+	#redireciona para a lista de usuários
+	header("Location: /admin/users");
+	exit;
+
+});
+#tela de esqueci senha
+$app->get("/admin/forgot",function(){
+
+	$page = new PageAdmin([
+			#desabilitando o header e o footer padrão
+			"header" => false,
+			"footer" => false
+		]);
+
+	$page->setTpl("forgot");
+});
+#
+$app->post("/admin/forgot",function(){
+	#pega o email que o usuário botou via post	
+	#método que faz as verificações
+	$user = User::getForgot($_POST["email"]);
+	#fazer um redrect pra confirmar pra pessoa que o email foi enviado com sucesso
+	header("Location:/admin/forgot/sent");
+	exit;
+});
+#tela de confirmação de email
+$app->get("/admin/forgot/sent", function(){
+
+	$page = new PageAdmin([
+			#desabilitando o header e o footer padrão
+			"header" => false,
+			"footer" => false
+		]);
+
+	$page->setTpl("forgot-sent");
+
+});
+#página de reset de senha
+$app->get("/admin/forgot/reset", function(){
+	#validar o código e recuperar de qual usuário pertence o código
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdmin([
+			#desabilitando o header e o footer padrão
+			"header" => false,
+			"footer" => false
+		]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
+
+});
+#resetar a senha
+$app->post("/admin/forgot/reset", function(){
+	#validar de novo o código
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+	#chama o método que informa que o processo de recuperação de senha já foi usado
+	User::setForgotUsed($forgot["idrecovery"]);
+	#carregando o objeto usuário
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+	#método pra salvar a senha do reset
+	$user->setPassword($_POST["password"]);
+	#mostrar a confirmação do reset
+	$page = new PageAdmin([
+			#desabilitando o header e o footer padrão
+			"header" => false,
+			"footer" => false
+		]);
+
+	$page->setTpl("forgot-reset-success");
+
+});
+
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,10 +4,13 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Mailer;
 
 class User extends Model{
 	#criando uma constante
 	const SESSION = "User";
+	#chave de criptografia 
+	const SECRET = "HcodePhp7_Secret";
 
 	#esse método vai verificar se o login e senha existe no banco 
 	public static function login($login, $password){
@@ -69,6 +72,191 @@ class User extends Model{
 		$_SESSION[User::SESSION] = NULL;
 
 	}
+	#criando o método de listar
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+		#virá os dados das duas tabelas ordenadas pelo nome das pessoas e retornar pra rota
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+	}
+	#método cadastrar
+	public function save()
+	{
+		#executar a query
+		$sql = new Sql();
+		#chamar uma procedure que vai inserir uma pessoa, precisamos saber o id dessa pessoa para poder inserir na tabela de usuários, pegar o id do usuário q retornou fazer um select com os dados juntar tudo e trazer de volta
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+
+		));
+		#só interessa o primeiro results
+		$this->setData($results[0]);
+
+	}
+
+	#busca as informações de um usuário que é chamada para atualizar o mesmo  
+	public function get($iduser)
+	{
+	 
+	 $sql = new Sql();
+	 
+	 $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+	 ":iduser"=>$iduser
+	 ));
+	 $this->setData($results[0]);
+	 #$data = $results[0];
+	 
+	 #$this->setData($data);
+	 
+	 }
+
+	 public function update()
+	 {
+	 	#executar a query
+		$sql = new Sql();
+		#chamar uma procedure que vai inserir uma pessoa, precisamos saber o id dessa pessoa para poder inserir na tabela de usuários, pegar o id do usuário q retornou fazer um select com os dados juntar tudo e trazer de volta
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+
+		));
+		#só interessa o primeiro results
+		$this->setData($results[0]);
+
+	 }
+	 #deletar
+	 public function delete()
+	 {
+
+	 	$sql = new Sql();
+
+	 	$sql->query("CALL sp_users_delete(:iduser)", array(
+	 		":iduser"=>$this->getiduser()
+	 	));
+
+	 }
+
+	#método statico forgot
+	public static function getForgot($email)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email;", array(
+			":email"=>$email
+		));
+
+		#valida se encontrou o email
+		if (count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha!");
+		}else
+		{
+			#guarda o id do usuário em $data
+			$data = $results[0];
+
+			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)",array(
+				":iduser"=>$data["iduser"],
+				":desip"=>$_SERVER["REMOTE_ADDR"] #tras o ip
+			));
+			#verificar se criou o results
+			if (count($results2) === 0) 
+			{
+				throw new \Exception("Não foi possível recuperar a senha!");
+						
+			}else
+			{
+				$dataRecovery = $results2[0];
+				#começa a criptografia da senha, primeiro vamos transformar em base 64
+				#caompos do mcrypt_encrypt (tipo de criptografia, chave de criptografia, dados que no caso encripta a chave primaria da tabela de recovry, tipo de criptografia vai usar)
+				$code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
+				#montar o link que será o endereço que vai receber o código
+				$link = "http://www.hcodecommerce.com.br:8080/admin/forgot/reset?code=$code";
+				#criando o email				
+				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir senha da Hcode Story", "forgot", array(
+					"name"=> $data["desperson"],
+					"link"=> $link
+				));
+				#enviar email
+				$mailer->send();
+				#retornando $data com os dados do usuário que foi recuperado, pra caso o método preciso pra alguma coisa
+				return $data;
+
+			}
+
+		}
+
+	} 
+	#Método para validar o código e recuperar de qual usuário pertence o código
+	public static function validForgotDecrypt($code)
+	{
+		#decriptando o code
+		$idrecovery = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, User::SECRET, base64_decode($code), MCRYPT_MODE_ECB);
+		
+		$sql = new Sql();
+		#select do banco já fazendo as validações
+		$results = $sql->select("
+			SELECT * 
+			FROM tb_userspasswordsrecoveries a 
+			INNER JOIN tb_users b USING(iduser) 
+			INNER JOIN tb_persons c USING(idperson) 
+			WHERE 
+			a.idrecovery = :idrecovery
+			AND 
+			a.dtrecovery IS NULL
+			AND
+			DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();", array(
+					":idrecovery"=>$idrecovery
+				));
+				#se trazer um resultado igual a 0 não resupera a senha
+				if (count($results) === 0)
+				{
+					throw new \Exception("Não foi possível recuperar a senha.");
+					
+				}else #retorna os resultados da posição 0
+				{
+
+					return $results[0];
+
+				}
+
+	}
+	#método que informa que o processo de recuperação de senha já foi usado
+	public static function setForgotUsed($idrecovery)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+			":idrecovery"=>$idrecovery
+		));
+
+	}
+	#método para atualizar a nova senha da recuperação de senha
+	public function setPassword($password)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+			":password"=>$password,
+			":iduser"=>$this->getiduser()#busca do atributo
+		));
+
+	}
+
 
 }
 
